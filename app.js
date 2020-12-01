52acc01d8e980c1ad894177991f94381482ffddb
Jafar Abdulrasoul <justjimmar@gmail.com>
2020-12-02 00:00:14 +0300

    added functionality to handle reterieving a thread recursively

--- app.js
+++ app.js
@@ -39,6 +39,56 @@ async function getThreadTweetsForTweetId(tweet_id) {
     }
 }
 
+async function getThreadTweetsForTweetIdRecursively(tail_tweet_id) {
+    const credentials = await getTwitterCredentials();
+    const token = credentials.bearer_token;
+    const api = new Twitter(token);
+
+    let tweet_id = tail_tweet_id;
+    let fullThread = [];
+    while (true) {
+        try {
+            const tweet = await api.getTweetWithTweetId(tweet_id);
+            fullThread.push(tweet);
+            const refTweets = tweet.data[0].referenced_tweets;
+            if (refTweets == undefined)
+                break;
+
+            tweet_id = refTweets.find((refTweet) => refTweet?.type == "replied_to")?.id;
+
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    }
+    return fullThread;
+}
+
+function cleanRecursiveThread(thread) {
+    let cleanThread = {
+        "data": [],
+        "includes": { "media": [], "users": [] },
+        "replies_meta": {
+            "newest_id": null,
+            "oldest_id": null,
+            "result_count": thread.length
+        }
+    };
+
+    thread.forEach(tweet => {
+        cleanThread.data.push(tweet.data[0]);
+        cleanThread.includes.media.push(...tweet.includes?.media || []);
+        cleanThread.includes.users.push(...tweet.includes?.users || []);
+    });
+
+    cleanThread.replies_meta.newest_id = cleanThread.data[0].id;
+    cleanThread.data = cleanThread.data.reverse();
+    cleanThread.includes.media = cleanThread.includes.media.reverse();
+    cleanThread.replies_meta.oldest_id = cleanThread.data[0].id;
+
+    return cleanThread;
+}
+
 function getMediaForKeys(mediaKeys, media) {
     if (typeof mediaKeys == "undefined" || typeof media == "undefined")
         return [];
@@ -65,19 +115,26 @@ function cleanTweetObject(tweet, mediaibrary) {
     return cleanedTweet;
 }
 
-const tweet_id = "1329854762926432257";
+const tweet_id = "1333834585118007300";
 
 (async () => {
+    // let fullThread = await getThreadTweetsForTweetIdRecursively(tweet_id);
+
+    let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadRecursive.json`));
+    let cleanThread = cleanRecursiveThread(fullThread);
+    console.log(cleanThread);
     // let fullThread = await getThreadTweetsForTweetId(tweet_id);
-    // fs.writeFile("tests/fixtures/threadVideo.json", JSON.stringify(fullThread));
+    fs.writeFile("tests/fixtures/threadRecursiveCleaned.json", JSON.stringify(cleanThread));
 
     // let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadMultiPictures.json`));
     // let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadTextOnly.json`));
-    let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadMixedMedia.json`));
 
-    let mediaibrary = extractMediaFromThread(fullThread);
-    let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaibrary));
 
-    console.log(threadClean)
+    // let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadMixedMedia.json`));
+
+    // let mediaibrary = extractMediaFromThread(fullThread);
+    // let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaibrary));
+
+    // console.log(threadClean)
     // process.exit();
 })();
