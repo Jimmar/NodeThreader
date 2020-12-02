2379392fee42509bada47ebe1ce064d4aaf78706
Jafar Abdulrasoul <justjimmar@gmail.com>
2020-12-02 14:16:33 +0300

    added more tests

--- test/unit/app_test.js
+++ test/unit/app_test.js
@@ -1,14 +1,23 @@
 import { deepStrictEqual } from "assert";
 import fs from "fs";
-import { cleanRecursiveThread } from "../../threader.js";
+import { cleanRecursiveThread, extractMediaFromThread } from "../../threader.js";
 
 const FIXTURES = "test/fixtures";
 
-describe("cleanRecursiveThread", async () => {
-  const dataInput = JSON.parse(fs.readFileSync(`${FIXTURES}/threadRecursive.json`));
-  const expectedOutput = JSON.parse(fs.readFileSync(`${FIXTURES}/threadRecursiveCleaned.json`));
+describe("cleanRecursiveThread", () => {
+  const dataInput = JSON.parse(fs.readFileSync(`${FIXTURES}/data_input/threadRecursive.json`));
+  const expectedOutput = JSON.parse(fs.readFileSync(`${FIXTURES}/expected_output/threadRecursiveCleaned.json`));
 
-  it("should equal the cleaned artifact", () => {
+  it("clean the thread", () => {
     deepStrictEqual(cleanRecursiveThread(dataInput), expectedOutput);
+  })
+})
+
+describe("extract media from thread", () => {
+  const dataInput = JSON.parse(fs.readFileSync(`${FIXTURES}/data_input/threadMultiPictures.json`));
+  const expectedOutput = JSON.parse(fs.readFileSync(`${FIXTURES}/expected_output/mediaThreadMultiPictures.json`));
+  
+  it("should extract media", () => {
+    deepStrictEqual(extractMediaFromThread(dataInput), expectedOutput);
   })
 })
