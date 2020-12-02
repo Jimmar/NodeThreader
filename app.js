ad1990f1832649089fe19a9d0c79493c71b56427
Jafar Abdulrasoul <justjimmar@gmail.com>
2020-12-02 17:52:42 +0300

    basic api endpoints

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-import { promises as fs } from "fs";
-import { cleanRecursiveThread, extractMediaFromThread, cleanTweetObject } from "./threader.js";
+import { cleanRecursiveThread, extractMediaFromThread, cleanTweetObject } from "./src/services/threader.js";
+import express from "express";
+
+const app = express();
+const port = 3000
+
+app.get("/", (req, res) => {
+    return res.send("Base page")
+})
+
+app.get("/api/thread/:thread_id", (req, res) => {
+    const thread_id = req.params?.thread_id;
+    return res.send("got id " + thread_id);
+})
+
+app.listen(port, () => {
+    console.log(`app running on port:${port}`)
+})
+
+
+
 
 const FIXTURES = "test/fixtures";
-
 const tweet_id = "1333834585118007300";
 
-(async () => {
+// (async () => {
 
-    // let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadRecursive.json`));
-    // fs.writeFile("tests/fixtures/threadRecursiveCleaned.json", JSON.stringify(cleanThread));
+//     // let fullThread = JSON.parse(await fs.readFile(`${FIXTURES}/threadRecursive.json`));
+//     // fs.writeFile("tests/fixtures/threadRecursiveCleaned.json", JSON.stringify(cleanThread));
 
-    let mediaibrary = extractMediaFromThread(fullThread);
-    let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaibrary));
+//     let mediaibrary = extractMediaFromThread(fullThread);
+//     let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaibrary));
 
-    console.log(threadClean)
-    // process.exit();
-})();
+//     console.log(threadClean)
+//     // process.exit();
+// })();
