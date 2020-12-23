5a8df8f961ff08e668dfe05635898dab948d3f2e
Jafar Abdulrasoul <justjimmar@gmail.com>
2020-12-24 00:57:19 +0300

    meh

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import { promises as fs } from 'fs';
 import { cleanTweetObject, extractMediaFromThread, getThreadTweetsForTweetId } from "./src/services/threader.js";
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.get("/", (req, res) => {
-    return res.send("Base page")
-})
+    return res.send("Base page");
+});
 
 app.get("/api/thread/:thread_id", async (req, res) => {
     const thread_id = req.params?.thread_id;
@@ -19,27 +19,27 @@ app.get("/api/thread/:thread_id", async (req, res) => {
     let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaLibrary));
 
     return res.send(JSON.stringify(threadClean));
-})
+});
 
 app.listen(port, () => {
-    console.log(`app running on port:${port}`)
-})
+    console.log(`app running on port:${port}`);
+});
 
 
-const FIXTURES = "test/fixtures";
-const tweet_id = "1341193051184611331";
+// const FIXTURES = "test/fixtures";
+// const tweet_id = "1341193051184611331";
 
-(async () => {
+// (async () => {
 
-    console.time('getThreadTweetsForTweetIdRecursively')
-    let fullThread = await getThreadTweetsForTweetId(tweet_id);
-    console.timeEnd('getThreadTweetsForTweetIdRecursively')
+//     console.time('getThreadTweetsForTweetIdRecursively')
+//     let fullThread = await getThreadTweetsForTweetId(tweet_id);
+//     console.timeEnd('getThreadTweetsForTweetIdRecursively')
 
-    await fs.writeFile(`${FIXTURES}/test.json`, JSON.stringify(fullThread));
+//     await fs.writeFile(`${FIXTURES}/test.json`, JSON.stringify(fullThread));
 
-    // let mediaLibrary = extractMediaFromThread(fullThread);
-    // let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaLibrary));
+//     // let mediaLibrary = extractMediaFromThread(fullThread);
+//     // let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaLibrary));
 
-    // console.log(fullThread)
-    // process.exit();
-})();
+//     // console.log(fullThread)
+//     // process.exit();
+// })();
