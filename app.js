ac28c49a9e882a07b8ec3144706b3520b0bd782c
Jafar Abdulrasoul <justjimmar@gmail.com>
2021-01-16 19:11:15 +0300

    using ejs in the app and separating the apis from the page requests

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { promises as fs } from 'fs';
 import { cleanTweetObject, extractMediaFromThread, getThreadTweetsForTweetId } from "./src/services/threader.js";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+app.use(express.static("src/static"));
+
 
 app.get("/", (req, res) => {
-    return res.send("Base page");
+    res.render("pages/index");
 });
 
-app.get("/api/thread/:thread_id", async (req, res) => {
+app.get("/thread/:thread_id", async (req, res) => {
     const thread_id = req.params?.thread_id;
     const fullThread = await getThreadTweetsForTweetId(thread_id);
     if (fullThread === undefined)
@@ -18,8 +23,22 @@ app.get("/api/thread/:thread_id", async (req, res) => {
     let mediaLibrary = extractMediaFromThread(fullThread);
     let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaLibrary));
 
+    res.render("pages/thread", { "thread": threadClean });
+});
+
+// =================== APIS ===================
+app.get("/api/thread/:thread_id", async (req, res) => {
+    const thread_id = req.params?.thread_id;
+    const fullThread = await getThreadTweetsForTweetId(thread_id);
+    if (fullThread === undefined)
+        return res.send(`{error: No thread for id ${thread_id}}`);
+
+    let mediaLibrary = extractMediaFromThread(fullThread);
+    let threadClean = fullThread.data.map((tweet) => cleanTweetObject(tweet, mediaLibrary));
+
     return res.send(JSON.stringify(threadClean));
 });
+// =================== APIS ===================
 
 app.listen(port, () => {
     console.log(`app running on port:${port}`);
