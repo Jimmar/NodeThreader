cb0dacd61bb1fea4a1e43435565084f9285f7fa7
Jafar Abdulrasoul <justjimmar@gmail.com>
2021-03-22 19:28:56 +0300

    Added expandUrl function

--- src/helper/twitter.js
+++ src/helper/twitter.js
@@ -1,3 +1,5 @@
+import { tall } from 'tall';
+
 export function validTwitterStatusUrl(url) {
     if (typeof url === 'string' || url instanceof String) {
         url = url.trim();
@@ -5,4 +7,14 @@ export function validTwitterStatusUrl(url) {
         return re.test(url);
     }
     return false;
+}
+
+export async function expandtcoUrl(url) {
+    try {
+        const expandedUrl = await tall(url, { maxRedirects: 1 });
+        return expandedUrl;
+    } catch (err) {
+        console.error('Error while expanding url', err);
+        return null;
+    }
 }
