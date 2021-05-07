26a0c5c191b3ce7267140868f774929855312048
Jafar Abdulrasoul <justjimmar@gmail.com>
2021-05-08 01:16:27 +0300

    app port from env

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", "src/views");
 app.use(cors())
