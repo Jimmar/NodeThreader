b4adc30024ca9da4052ca7944a5b8eb168ae15a3
Jimmar <justjimmar@gmail.com>
2022-02-06 17:21:30 +0300

    [Client] Code reformat

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,32 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Thread from '../views/Thread.vue'
-import NotFound from '../views/NotFound.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Thread from "../views/Thread.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/thread/:threadId',
-    name: 'Thread',
+    path: "/thread/:threadId",
+    name: "Thread",
     props: true,
     component: Thread
   },
   {
-    path: '/:pathMatch(.*)*',
-    // name: 'NotFound', 
+    path: "/:pathMatch(.*)*",
+    // name: 'NotFound',
     // component: NotFound
     //TODO is this better or 404 ?
-    redirect: '/'
-  },
-]
+    redirect: "/"
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = "Node Threader";
-        return args;
-      })
+    config.plugin("html").tap(args => {
+      args[0].title = "Node Threader";
+      return args;
+    });
   },
   publicPath: "/NodeThreader",
   outputDir: "dist"
-}
+};
